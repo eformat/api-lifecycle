c34cb983cfba475d4dce3d93b287f481bc76592a
Mike Hepburn <eformat@gmail.com>
2019-05-28 14:40:55 +1000

    updated for offline install

--- README.md
+++ README.md
@@ -3,9 +3,15 @@
 - Check inventory for variables.
 - Working `oc` login.
 
- ```
+Install
+
+```
 ansible-playbook -i workshop.inventory install.yml
- ```
+```
+Uninstall
+```
+ansible-playbook -i workshop.inventory uninstall.yml
+```
 
 Provisions:
 
@@ -13,3 +19,5 @@ Provisions:
 - 3Scale API Management
 - Apicurio
 - Microcks
+
+![api-lifecycle](images/api-lifecycle-1024x709.png)
---
+++ templates/get-template.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/jboss-container-images/redhat-sso-7-openshift-image/sso72-dev/templates/sso72-mysql-persistent.json
+wget https://raw.githubusercontent.com/jboss-container-images/redhat-sso-7-openshift-image/sso72-dev/templates/sso72-image-stream.json
+wget https://raw.githubusercontent.com/jboss-openshift/application-templates/ose-v1.4.18/secrets/sso-app-secret.json
+wget https://raw.githubusercontent.com/jbossdemocentral/3scale-api-workshop/master/support/templates/realm-template.json
+wget https://raw.githubusercontent.com/3scale/3scale-amp-openshift-templates/master/amp/amp.yml
+wget https://raw.githubusercontent.com/jbossdemocentral/3scale-api-workshop/master/support/templates/apicurio-template.yml
+wget https://raw.githubusercontent.com/jbossdemocentral/3scale-api-workshop/master/support/templates/apicurio-realm.json
+wget https://raw.githubusercontent.com/eformat/3scale-api-workshop/master/support/templates/microcks-template.yml
+wget https://raw.githubusercontent.com/jbossdemocentral/3scale-api-workshop/master/support/templates/microcks-realm.json
+wget https://raw.githubusercontent.com/OpenShiftDemos/gogs-openshift-docker/master/openshift/gogs-template.yaml
+wget https://raw.githubusercontent.com/RedHatWorkshops/dayinthelife-integration/master/support/templates/gogs.json
+wget https://raw.githubusercontent.com/RedHatWorkshops/dayinthelife-integration/master/support/templates/gogs-www.json
